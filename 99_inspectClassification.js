54f956a0cd8ec6cbc1cd8f16dc2e621b4c0c22fa
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2021-09-27 14:39:25 -0300

    Update 99_inspectClassification.js

--- 99_inspectClassification.js
+++ 99_inspectClassification.js
@@ -5,6 +5,7 @@ var year = 2020;
 // define inputs
 var mosaic = 'projects/nexgenmap/MapBiomas2/SENTINEL/mosaics';
 var classification = 'projects/mapbiomas-workspace/AUXILIAR/CERRADO/SENTINEL/classification_sentinel';
+var col6 = ee.Image('projects/mapbiomas-workspace/public/collection6/mapbiomas_collection60_integration_v1');
 
 // import the color ramp module from mapbiomas 
 var palettes = require('users/mapbiomas/modules:Palettes.js');
@@ -24,8 +25,12 @@ var mosaic = ee.ImageCollection(mosaic)
 // import classification
 var classification = ee.ImageCollection(classification)
     .filterMetadata('year', 'equals', year)
-    .filterMetadata('version', 'equals', '1')
+    .filterMetadata('version', 'equals', 1)
     .mosaic();
+
+// import col6
+var col6 = col6.select(['classification_' + year])
+            .updateMask(classification);
 
 // plot sentinel mosaic
 Map.addLayer(mosaic, {
@@ -33,6 +38,9 @@ Map.addLayer(mosaic, {
     'gain': [0.08, 0.07, 0.2],
     'gamma': 0.85
 }, 'Sentinel ' + year);
+
+// plot col6
+Map.addLayer(col6, vis, 'col6 ' + year);
 
 // plot classification 
 Map.addLayer(classification, vis, 'classification ' + year);
