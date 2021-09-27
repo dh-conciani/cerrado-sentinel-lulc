b04d4bec9aff2222a9c737abd171cb8bab3bc208
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2021-09-27 15:06:50 -0300

    Update 99_inspectClassification.js

--- 99_inspectClassification.js
+++ 99_inspectClassification.js
@@ -6,6 +6,7 @@ var year = 2020;
 var mosaic = 'projects/nexgenmap/MapBiomas2/SENTINEL/mosaics';
 var classification = 'projects/mapbiomas-workspace/AUXILIAR/CERRADO/SENTINEL/classification_sentinel';
 var col6 = ee.Image('projects/mapbiomas-workspace/public/collection6/mapbiomas_collection60_integration_v1');
+var landsat =  'projects/nexgenmap/MapBiomas2/LANDSAT/mosaics';
 
 // import the color ramp module from mapbiomas 
 var palettes = require('users/mapbiomas/modules:Palettes.js');
@@ -31,7 +32,14 @@ var classification = ee.ImageCollection(classification)
 // import col6
 var col6 = col6.select(['classification_' + year])
             .updateMask(classification);
-
+            
+// import landsat mosaic
+var landsat = ee.ImageCollection(landsat)
+    .filterMetadata('year', 'equals', year)
+    .filterMetadata('biome', 'equals', 'CERRADO')
+    .filterMetadata('version', 'equals', '2')
+    .mosaic();
+    
 // plot sentinel mosaic
 Map.addLayer(mosaic, {
     'bands': ['swir1_median', 'nir_median', 'red_median'],
@@ -39,8 +47,16 @@ Map.addLayer(mosaic, {
     'gamma': 0.85
 }, 'Sentinel ' + year);
 
+// Plot Landsat
+Map.addLayer(landsat, {
+        bands: ['swir1_median', 'nir_median', 'red_median'],
+        gain: [0.08, 0.07, 0.2],
+        gamma: 0.85
+    },
+    'Landsat ' + year);
+
 // plot col6
-Map.addLayer(col6, vis, 'col6 ' + year);
+Map.addLayer(col6, vis, 'Collection 6.0 ' + year);
 
 // plot classification 
-Map.addLayer(classification, vis, 'classification ' + year);
+Map.addLayer(classification, vis, 'Collection Sentinel ' + year);
