981c7c01f57b332e636e9ae199f763543f50a669
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2021-09-29 15:24:07 -0300

    Update 05_filterOutliers.py

--- 05_filterOutliers.py
+++ 05_filterOutliers.py
@@ -21,16 +21,16 @@ bandnames = ['ndvi_median'];
 version = '1'
 
 ## define regions to be filtered
-regions = [ 1, 2, 3  
-            #1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 
-            #12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-            #23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 
-            #34, 35, 36, 37, 38
+regions = [ 
+            1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 
+            12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+            23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 
+            34, 35, 36, 37, 38
             ]
 
 ## define years to be filtered
 years = [
-        2020
+        2016, 2017, 2018, 2019, 2020
         ]
 
 ## define classes to be filtered
@@ -117,5 +117,3 @@ stat_df = pd.DataFrame(data= stat_df)
 stat_df.to_csv(path_or_buf='data.csv')     
 print ('statistics exported: root ./')
 print ('done :)')
-
-
