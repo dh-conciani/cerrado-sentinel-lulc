64560e6870f0110be71604f67d7117d254981f4a
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2021-10-08 18:24:36 -0300

    Create 11_postFrequency.js

---
+++ 11_postFrequency.js
@@ -0,0 +1,79 @@
+// frequency filter - apply only in pixels that changed between native vegetation classes
+
+// define settings
+var bioma = "CERRADO";
+var file_in = bioma + '_sentinel_gapfill_wetland_temporal_spatial_v1';
+var file_out = bioma + '_sentinel_gapfill_wetland_temporal_spatial_freq_v';
+var dirout = 'projects/mapbiomas-workspace/AUXILIAR/CERRADO/SENTINEL/classification_sentinel/';
+var version_out = 1;
+
+// import classification
+var class4 = ee.Image(dirout + file_in);
+
+// define mapbiomas color rmap
+var vis = {
+      bands: 'classification_2019',
+      min: 0,
+      max: 49,
+      palette: require('users/mapbiomas/modules:Palettes.js').get('classification6'),
+      format: 'png'
+    };
+
+// compute frequency 
+var filtrofreq = function(mapbiomas){
+  // calc expression 
+ var exp = '100*((b(0)+b(1)+b(2)+b(3)+b(4)/5))';
+  
+  // get frequency
+  var florFreq = mapbiomas.eq(3).expression(exp);
+  var savFreq = mapbiomas.eq(4).expression(exp);
+  var wetFreq = mapbiomas.eq(11).expression(exp);
+  var grassFreq = mapbiomas.eq(12).expression(exp);
+
+  // select pixels that were native vegetation at least [x]% of the time series
+  var vegMask = ee.Image(0).where((florFreq.add(savFreq).add(wetFreq).add(grassFreq)).gte(79), 1);
+
+  // define rules per class
+  var  vegMap = ee.Image(0)
+                          .where(vegMask.eq(1).and(wetFreq.gt(60)), 11)
+                          .where(vegMask.eq(1).and(grassFreq.gt(60)), 12)
+                          .where(vegMask.eq(1).and(savFreq.gt(60)), 4)
+                          .where(vegMask.eq(1).and(florFreq.gt(20)), 3);
+  
+  // update with results
+  vegMap = vegMap.updateMask(vegMap.neq(0));
+  //NaovegMask = NaovegMask.updateMask(NaovegMask.neq(0))//.clip(BiomaPA)
+  //Map.addLayer(vegMap, vis, 'vegetacao estavel', true);
+  //Map.addLayer(NaovegMask, vis, 'Não vegetacao estavel', true);
+  
+  var saida = mapbiomas.where(vegMap, vegMap);
+  //saida = saida.where(NaovegMask, NaovegMask)
+  
+  return saida;
+};
+
+  var saida = filtrofreq(class4);
+  saida = saida
+    .set('version', version_out)
+    .set('biome', bioma)
+    .set('step', 'frequency');
+
+print('classification', class4);
+print('filtered', saida);
+
+// plot results
+Map.addLayer(class4, vis, 'classification');
+Map.addLayer(saida, vis, 'filtered');
+
+// export as GEE asset
+Export.image.toAsset({
+    'image': saida,
+    'description': file_out + version_out,
+    'assetId': dirout + file_out + version_out,
+    'pyramidingPolicy': {
+        '.default': 'mode'
+    },
+    'region': saida.geometry(),
+    'scale': 30,
+    'maxPixels': 1e13
+});
