6383a3ff3205f1483953d1e5395035176b02fcc4
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2021-11-04 16:38:50 -0300

    Update 03b_createSegments.js

--- 03b_createSegments.js
+++ 03b_createSegments.js
@@ -1,12 +1,20 @@
 // remove outliers from stable pixels by using segmentation 
 // dhemerson.costa@ipam.org.br
 
+// cerrado extent
+var cerrado_extent = ee.Geometry.Polygon(
+        [[[-62.30982042125685, -0.9217170277068568],
+          [-62.30982042125685, -25.759333772762382],
+          [-39.63403917125685, -25.759333772762382],
+          [-39.63403917125685, -0.9217170277068568]]], null, false);
+
 // import datasets 
 // stable samples 
 var stable_pixels = ee.Image('projects/mapbiomas-workspace/AUXILIAR/CERRADO/CE_amostras_estaveis85a20_col6_v2');
 
 // sample points (un-filtered)
 var sample_points = ee.FeatureCollection('projects/mapbiomas-workspace/AMOSTRAS/Cerrado/col6/samples-planet/samples_col6_CERRADO_v21');
+print ('number of sample points: ', sample_points.size());
 
 // sentinel mosaic for the year of 2020 
 var sentinel = ee.ImageCollection('projects/nexgenmap/MapBiomas2/SENTINEL/mosaics')
@@ -60,26 +68,7 @@ var sample_buffer = sample_points.map(getBuffer);
 
 // plot buffers
 Map.addLayer(sample_buffer, {}, 'buffer', false);
-
-/* vectorize segments within each buffer 
-var vecSegments = function (obj) {
-  // clip segments 
-  var buffered_segment = segments.select(['clusters']).clip(obj);
-  // vectorize segments
-  return(buffered_segment.reduceToVectors({
-          crs: buffered_segment.projection(),
-          scale: 10,
-          geometryType: 'polygon',
-          eightConnected: false,
-          labelProperty: 'clusters',
-          maxPixels: 60366972215
-    }).flatten()
-  );
-};
-*/
-
-// execute function to vectorize segments within buffers
-// var vectorized_segments = sample_buffer.map(vecSegments);
+print ('number of buffers produced: ', sample_buffer.size());
 
 // clip segments using the buffer
 var clipped_segments = segments.select(['clusters']).clipToCollection(sample_buffer);
@@ -87,4 +76,28 @@ var clipped_segments = segments.select(['clusters']).clipToCollection(sample_buf
 // plot clipped segments
 Map.addLayer(clipped_segments.randomVisualizer(), {}, 'clipped segments');
 
+// vectorize segments within each buffer 
+var vecSegments = function (obj) {
+  // vectorize segments
+  return(clipped_segments.reduceToVectors({
+          geometry: obj.geometry(),
+          crs: clipped_segments.projection(),
+          scale: 10,
+          geometryType: 'polygon',
+          eightConnected: false,
+          labelProperty: 'clusters',
+          maxPixels: 60366972215
+    })
+  );
+};
 
+// execute function to vectorize segments within buffers
+var vectorized_segments = sample_buffer.map(vecSegments);
+print ('number of vectors produced: ', vectorized_segments.flatten().size());
+
+// select only segments that overlaps the sample points
+//var selected_segments = vectorized_segments.intersection(sample_points);
+//print ('number of filtered segments: ', selected_segments.size());
+
+// plot vectors 
+Map.addLayer(vectorized_segments.flatten(), {}, 'vectors');
