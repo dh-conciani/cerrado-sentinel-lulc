24cffa078638765c3615f97f6065ea4d2ea08bab
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2022-10-17 12:33:38 -0300

    Create rasterize.js

---
+++ utils/classification_regions/rasterize.js
@@ -0,0 +1,45 @@
+// get vector
+var table = ee.FeatureCollection("users/dh-conciani/collection7/classification_regions/vector_v2");
+
+// rasterize
+var r = table
+  .reduceToImage({
+    properties: ['mapb'],
+    reducer: ee.Reducer.min() // apply minimum reducer 
+});
+
+// plot
+Map.addLayer(r.randomVisualizer(), {}, 'classification regions');
+
+// export 10m image
+Export.image.toAsset({
+    'image': r,
+    'description': 'raster_10m_v2',
+    'assetId': 'users/dh-conciani/collection7/classification_regions/raster_10m_v2',
+    'pyramidingPolicy': {
+        '.default': 'mode'
+    },
+    'region': geometry,
+    'scale': 10,
+    'maxPixels': 1e13
+});
+
+// export each region as a image 
+ee.List.sequence({'start': 1, 'end': 38}).getInfo().forEach(function(reg_i) {
+  // get region i (img)
+  var x = r.updateMask(r.eq(reg_i))//.aside(Map.addLayer);
+  // get region i (geometry)
+  var y = table.filterMetadata('mapb', 'equals', reg_i)//.aside(Map.addLayer);
+  // export 
+  Export.image.toAsset({
+    'image': r,
+    'description': 'reg_' + reg_i,
+    'assetId': 'users/dh-conciani/collection7/classification_regions/eachRegion_v2_10m/reg_' + reg_i,
+    'pyramidingPolicy': {
+        '.default': 'mode'
+    },
+    'region': y.geometry(),
+    'scale': 10,
+    'maxPixels': 1e13
+  });
+})
