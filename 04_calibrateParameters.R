f0e69908ac82858e79162b5f578bb00c9910b0f1
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2022-10-21 13:00:47 -0300

    Update 04_calibrateParameters.R

--- 04_calibrateParameters.R
+++ 04_calibrateParameters.R
@@ -61,8 +61,10 @@ for (i in 1:length(region_name)) {
     clip(regions$filterMetadata('mapb', 'equals', region_name[i]))$rename('hand')
   
   ## get spectral signatures for a random year (repeat two times, using two random years)
-  for (j in 1:2) {
-    print(paste0('year ', j, ' of 2'))
+  n_years <- 2
+  
+  for (j in 1:n_years) {
+    print(paste0('year ', j, ' of ', n_years))
     
     ## filter sentinel mosaic for a random year
     sentinel_ij <- sentinel_i$filter(ee$Filter$eq('year', sample(x= 2016:2021, size= 1)))$mosaic()$
@@ -77,122 +79,154 @@ for (i in 1:length(region_name)) {
                                                   scale= 10,
                                                   geometries= TRUE,
                                                   tileScale= 2), via = 'drive'))
+    
+    
+    ## remove description columns 
+    sample_ij <- sample_ij[ , -which(names(sample_ij) %in% c("id","mapb", "geometry"))]
+    
+    ## set RF heuristic learning functions
+    customRF <- list(type = "Classification",
+                     library = "randomForest",
+                     loop = NULL)
+    
+    ## set parameters to be optimized 
+    customRF$parameters <- data.frame(parameter = c("mtry", "ntree"),
+                                      reference = rep("numeric", 2),
+                                      label = c("mtry", "ntree"))
+    
+    ## set searching method 
+    customRF$grid <- function(x, y, len = NULL, search = "grid") {}
+    
+    ## set training function 
+    customRF$fit <- function(x, y, wts, param, lev, last, weights, classProbs) {
+      randomForest(x, y,
+                   mtry = param$mtry,
+                   ntree=param$ntree)
+    }
+    
+    ## set prediction function 
+    customRF$predict <- function(modelFit, newdata, preProc = NULL, submodels = NULL)
+      predict(modelFit, newdata)
+    
+    ## set data-structure functions 
+    customRF$sort <- function(x) x[order(x[,1]),]
+    customRF$levels <- function(x) x$reference
+    
+    ## set repeated cross-validation folds
+    dcv_n <- 10
+    dcv_rep <- 3
+    
+    ## set train control 
+    control <- trainControl(method="repeatedcv", 
+                            number= dcv_n, 
+                            repeats= dcv_rep,
+                            allowParallel = TRUE)
+    
+    ## set a grid of parameters to be tested (half od default, default and double)
+    tunegrid <- expand.grid(.mtry=c(sqrt(ncol(sample_ij))/2, sqrt(ncol(sample_ij)), sqrt(ncol(sample_ij))*2),
+                            .ntree=c(100, 300))
+    
+    ## standardize seed
+    set.seed(1)
+    
+    ## define the number of model repetitions in each year
+    n_rep <- 3
+    
+    ## run n times
+    for (k in 1:n_rep) {
+      print(paste0('training model ', k, ' of ', n_rep))
+      ## create a sub sample of x percent
+      p <- 5
+      
+      ## perform sub sample
+      sub_sample <- sample_ij[sample(x= 1: nrow(sample_ij),
+                                     size = round(nrow(sample_ij) / 100 * p)) ,]
+      
+      ## train model 
+      custom <- train(as.factor(reference)~.,
+                      data= sub_sample, 
+                      method= customRF, 
+                      metric= 'Accuracy', 
+                      tuneGrid= tunegrid, 
+                      trControl= control)
+      
+      ## in the first iteration, build objects
+      if (exists('tune') == FALSE) {
+        ## get best parameters
+        tune <- custom$bestTune
+        imp <- varImp(custom, scale= TRUE)$importance
+        imp$band <- row.names(imp)
+      } else {
+        ## get best parameters
+        ## add
+        tune <- rbind(tune, custom$bestTune)
+        imp_x <- varImp(custom, scale= TRUE)$importance
+        imp_x$band <- row.names(imp_x)
+        imp <- rbind(imp, imp_x)
+      }
+      
+    } ## end of sub-sample repetition
+    
+    ## aggregate statistics for tuning parameters
+    tune_file_ij <- as.data.frame(cbind(mtry= round(mean(tune$mtry)), 
+                                        ntree= round(mean(tune$ntree)), 
+                                        region = region_name[i],
+                                        year = j))
+    
+    ## compile all years of each region
+    if (exists('tune_y') == FALSE) {
+      tune_y <- tune_file_ij
+    } else {
+      tune_y <- rbind(tune_y, tune_file_ij)
+    }
+    
+    ## aggregate statistics for variable importance
+    bands_file_ij <- as.data.frame(cbind(band= unique(imp$band),
+                                         sum= aggregate(x=list(sum = imp$Overall), by=list(band= imp$band), FUN= 'sum')$sum,
+                                         mean= aggregate(x=list(mean = imp$Overall), by=list(band= imp$band), FUN= 'mean')$mean,
+                                         sd= aggregate(x=list(sd = imp$Overall), by=list(band= imp$band), FUN= 'sd')$sd,
+                                         region= region_name[i],
+                                         year= j))
+    
+    ## compile all years of each region
+    if (exists('bands_y') == FALSE) {
+      bands_y <- bands_file_ij
+    } else {
+      bands_y <- rbind(bands_y, bands_file_ij)
+    }
+
+
+  } ## end of years loop
+ 
+  ## aggregate statistics for tuning parameters
+  tune_file_i <- as.data.frame(cbind(mtry= round(mean(tune_y$mtry)), 
+                                     ntree= round(mean(tune_y$ntree)), 
+                                     region = region_name[i]))
+  
+  ## insert into database
+  ## rf parameters
+  if (exists('tune_xx') == FALSE) {
+    tune_xx <- tune_file_i
+  } else {
+    tune_xx <- rbind(tune_xx, tune_file_i)
   }
   
+  ## aggregate region metrics
+  bands_file_i <- as.data.frame(cbind(band= unique(bands_y$band),
+                                      sum= aggregate(x=list(sum = as.numeric(bands_y$sum)), by=list(band= bands_y$band), FUN= 'mean')$sum,
+                                      mean= aggregate(x=list(mean = as.numeric(bands_y$mean)), by=list(band= bands_y$band), FUN= 'mean')$mean,
+                                      sd= aggregate(x=list(sd = as.numeric(bands_y$sd)), by=list(band= bands_y$band), FUN= 'mean')$sd,
+                                      region= region_name[i]))
   
-  
-  
-  
-  
-}
+  ## variable importance
+  if (exists('bands_xx') == FALSE) {
+    bands_xx <- bands_file_i
+  } else {
+    bands_xx <- rbind(bands_xx, bands_file_i)
+  }
 
+} ## end of regions loop
 
-
-## get sentinel only for region [i]
-sentinel_i <- sentinel$filterBounds(regions$filterMetadata('mapb', 'equals', 37))
-
-## get sample points for the region [i]
-samples_i <- samples$filterMetadata('mapb', 'equals', 37)
-
-## compute additional bands
-geo_coordinates <- ee$Image$pixelLonLat()$
-  clip(regions$filterMetadata('mapb', 'equals', 37))
-
-## get latitude
-lat <- geo_coordinates$select('latitude')$add(5)$multiply(-1)$multiply(1000)$toInt16()
-## get longitude
-lon_sin <- geo_coordinates$select('longitude')$multiply(pi)$divide(180)$
-  sin()$multiply(-1)$multiply(10000)$toInt16()$rename('longitude_sin')
-## cosine
-lon_cos <- geo_coordinates$select('longitude')$multiply(pi)$divide(180)$
-  cos()$multiply(-1)$multiply(10000)$toInt16()$rename('longitude_cos')
-
-## get heigth above nearest drainage
-hand <- ee$ImageCollection("users/gena/global-hand/hand-100")$mosaic()$toInt16()$
-  clip(regions$filterMetadata('mapb', 'equals', 37))$
-  rename('hand')
-
-#LOOP HERE
-## sort a year
-sentinel_i <- sentinel_i$filter(ee$Filter$eq('year', sample(x= 2016:2021, size= 1)))$mosaic()
-
-# bind auxiliary bands into sentinel mosaic
-sentinel_ij <- sentinel_i$addBands(lat)$
-  addBands(lon_sin)$
-  addBands(lon_cos)$
-  addBands(hand)
-
-## select year
-## get spectral signatures 
-sample_ij <- as.data.frame(na.omit(ee_as_sf(sentinel_ij$
-                                            sampleRegions(collection= samples_i,
-                                                          scale= 10,
-                                                          geometries= TRUE,
-                                                          tileScale= 2), via = 'drive')))
-
-## remove description columns 
-sample_ij <- sample_ij[ , -which(names(sample_ij) %in% c("id","mapb", "geometry"))]
-
-## set RF heuristic learning functions
-customRF <- list(type = "Classification",
-                 library = "randomForest",
-                 loop = NULL)
-
-## set parameters to be optimized 
-customRF$parameters <- data.frame(parameter = c("mtry", "ntree"),
-                                  reference = rep("numeric", 2),
-                                  label = c("mtry", "ntree"))
-
-## set searching method 
-customRF$grid <- function(x, y, len = NULL, search = "grid") {}
-
-## set training function 
-customRF$fit <- function(x, y, wts, param, lev, last, weights, classProbs) {
-  randomForest(x, y,
-               mtry = param$mtry,
-               ntree=param$ntree)
-}
-
-## set prediction function 
-customRF$predict <- function(modelFit, newdata, preProc = NULL, submodels = NULL)
-  predict(modelFit, newdata)
-
-## set data-structure functions 
-customRF$sort <- function(x) x[order(x[,1]),]
-customRF$levels <- function(x) x$reference
-
-## set train control 
-control <- trainControl(method="repeatedcv", 
-                        number=10, 
-                        repeats=3,
-                        allowParallel = TRUE)
-
-## set a grid of parameters to be tested (half od default, default and double)
-tunegrid <- expand.grid(.mtry=c(sqrt(ncol(sample_ij))/2, sqrt(ncol(sample_ij)), sqrt(ncol(sample_ij))*2),
-                        .ntree=c(100, 300))
-
-## standardize seed
-set.seed(1)
-
-## create a sub sample of x percent
-p <- 10
-
-## run n times
-#for (k in 1:20) {
-#  
-#}
-
-## perform sub sample
-sub_sample <- sample_ij[sample(x= 1: nrow(sample_ij),
-                               size = round(nrow(sample_ij) / 100 * p)) ,]
-
-## train model 
-custom <- train(as.factor(reference)~.,
-                data= sub_sample, 
-                method= customRF, 
-                metric= 'Accuracy', 
-                tuneGrid= tunegrid, 
-                trControl= control)
-
-
-
+## export tables
+write.table(bands_xx, file = './_params/bands.csv')
+write.table(tune_xx, file = './_params/rf.csv')
