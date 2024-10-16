28c399b514c09bf5a9949c33d564e9fb8cc2b0c5
Bárbara Costa <132362599+costa-barbara@users.noreply.github.com>
2024-10-16 10:28:35 -0300

    Update 05_calibrateParameters.R

--- 05_calibrateParameters.R
+++ 05_calibrateParameters.R
@@ -1,35 +1,35 @@
-## -- -- -- -- 03b_calibrateParameters
-## select featureSpace and calibrate RF hyperparameters for each classification region.
-## dhemerson.costa@ipam.org.br
+## -- -- -- -- 05_calibrateParameters
+## Select feature space and calibrate Random Forest (RF) hyperparameters for classification.
+## dhemerson.costa@ipam.org.br and babara.silva@ipam.org.br
 
-## load packages
+## Load required packages
 library(rgee)
 library(tidyverse)
 
-## avoidtidyr## avoid scientific notation
+## Avoid scientific notation in numbers
 options(scipen= 9e3)
 
-## initialize earth engine 
+## Initialize Earth Engine
 ee_Initialize()
 
-## set the version of training samples to used
+## Set the version of the training samples to be used
 version <- "5"
 
-## set training folder 
+## Set the training data folder path
 folder <- paste0('projects/mapbiomas-workspace/COLECAO_DEV/COLECAO9_DEV/CERRADO/SENTINEL_DEV/training/v', version, '/')
 
-## read classification regions
+## Load the classification regions
 regions <- ee$FeatureCollection('users/dh-conciani/collection7/classification_regions/vector_v2')
 
-################## set sample size functions ##############################
-## get the number of years to be used
+## --- --- Set sample size and selection functions
+## Function to calculate the number of years based on a proportion
 yearSize <- function(end_year, start_year, proportion) {
   return (
-    round((end_year - start_year + 1)/ 100 * proportion, digits=0)
+    round((end_year - start_year + 1) / 100 * proportion, digits=0)
   )
 }
 
-## get years to be used in calibration 
+## Function to randomly select years based on a proportion
 getYears <- function(start_year, end_year, proportion) {
   return(
     sample(x= start_year:end_year, 
@@ -37,138 +37,149 @@ getYears <- function(start_year, end_year, proportion) {
   )
 }
 
-## set grid of parameters to be tested
+## Create a grid of parameters (ntree, mtry, region, year) to test
 combinations <- expand.grid(
-  ntree=c(100, 200, 300),
-  mtry=c(4, 8, 12),
-  region=c(1:38),
-  year=c(2016:2023)
+  ntree = c(100, 200, 300),  ## Number of trees
+  mtry = c(4, 8, 12),        ## Variables to consider at each split
+  region = c(1:38),          ## Classification region IDs
+  year = c(2016:2023)        ## Years for calibration
 )
 
-################# check already processed files ################## comment if is the first run 
+## --- --- Check for already processed files 
+## Comment if is the first run 
+## Read already processed parameters to avoid reprocessing
 processedTable <- read.csv('./_aux/temp/modelParams.csv', sep=' ')
 
-# Find combinations that are not present in processedTable
+## Filter combinations to exclude the ones already processed
 combinations <- anti_join(combinations, processedTable, by = c("ntree", "mtry", "region", "year"))
-######################################################################################################
+## ---
 #combinations <- combinations[1:2000,]
-###################
 
-## set recipes
+
+## Initialize tables to store parameter results and variable importance
 paramTable <- as.data.frame(NULL)
 importanceTable <- as.data.frame(NULL)
 
-## set count
+## Initialize a counter for progress tracking
 count <- 0
 
-## for each region 
+## Loop over each unique classification region
 for (i in 1:length(unique(combinations$region))) {
-  print(paste0('processing region ', unique(combinations$region)[i],' --- ',
+  print(paste0('Processing region ', unique(combinations$region)[i],' --- ', 
                i, ' of ', length(unique(combinations$region))))
   
-  ## sort random years to calibrate parameters 
-  # set_of_years <- getYears(start_year= 1985,
+  ## Sort random years to calibrate parameters 
+  # set_of_years <- getYears(start_year= 2016,
   #                          end_year= 2023, 
   #                          proportion= 20)
   
-  ## for each classification region
+  ## Loop over each year
   for (j in 1:length(unique(combinations$year))) {
-    print(paste0('year ', j, ' of ', length(unique(combinations$year)), ' ----> ', unique(combinations$year)[j]))
+    print(paste0('Year ', j, ' of ', length(unique(combinations$year)), 
+                 ' ----> ', unique(combinations$year)[j]))
+
+
+  ## Loop over each year for the given region
+  for (j in 1:length(unique(combinations$year))) {
+    print(paste0('Year ', j, ' of ', length(unique(combinations$year)), 
+                 ' ----> ', unique(combinations$year)[j]))
     
-    ## read training samples for the region [i] and year [j]
+    ## Load training samples for the selected region [i] and year [j]
     samples_ij <- ee$FeatureCollection(paste0(folder, 'train_col9_reg', unique(combinations$region)[i],
                                               '_', unique(combinations$year)[j], '_v', version))
-    ## get bandNames
+    
+    ## Get the band names
     bands <- names(samples_ij$first()$getInfo()$properties)
     
-    ## remove descriptots
+    ## Remove descriptors (e.g., 'mapb', 'year')
     bands <- bands[!bands %in% c('mapb', 'year')]
     
-    ## get combinations for region 
+    ## Get the combinations for the specific region and year
     try(combinations_k <- subset(combinations, region == unique(combinations$region)[i] &
                                    year == unique(combinations$year)[j]), silent = TRUE)
     
+   ## Skip if no combinations are available for the selected region-year pair
     if (nrow(combinations_k) == 0) {
       count <- count + 1
       next
-      
     }
-    ## for each combination in search grid
-    for(k in 1:nrow(combinations_k)) {
-      ## set count
+    
+    ## Loop over each parameter combination for the region-year pair
+    for (k in 1:nrow(combinations_k)) {
+      ## Update the progress counter
       count <- count + 1
-      print(paste0('training combination ', k, ' of ', nrow(combinations_k), 
+      print(paste0('Training combination ', k, ' of ', nrow(combinations_k), 
                    ' ~ iteration ', count, ' of ', nrow(combinations)))
       
-      ## store initializing time
+      ## Record the starting time of the training process
       startTime <- Sys.time()
       
-      ## separate into training and test 
+      ## Split the samples into training (70%) and testing (30%) sets
       samples_ij <- samples_ij$randomColumn()
       samples_ij_training <- samples_ij$filter('random <= 0.7')
       samples_ij_test <- samples_ij$filter('random > 0.8')
       
-      # train a smile.randomForest 
+      ## Train a Random Forest classifier using the selected hyperparameters
       trainedClassifier <- ee$Classifier$smileRandomForest(
         numberOfTrees = combinations_k[k,]$ntree,
         variablesPerSplit = combinations_k[k,]$mtry
       )$train(
-        features= samples_ij_training,
-        classProperty= 'reference',
-        inputProperties= bands
+        features = samples_ij_training,
+        classProperty = 'reference',
+        inputProperties = bands
       )
       
-      ## get a confusion matrix and overall accuracy for the test sample.
+      ## Classify the testing set and calculate the confusion matrix and accuracy
       samples_ij_test <- samples_ij_test$classify(trainedClassifier)
-      testAccuracy <- samples_ij_test$errorMatrix('reference', 'classification');
+      testAccuracy <- samples_ij_test$errorMatrix('reference', 'classification')
       
       print('Getting model results')
-      ## build accuracy table
+      
+      ## Store model accuracy for the current parameter combination
       try(tempParam <- as.data.frame(rbind(cbind(
-        ntree= combinations_k[k,]$ntree,
-        mtry= combinations_k[k,]$mtry,
-        region= unique(combinations$region)[i],
-        year= unique(combinations$year)[j],
-        accuracy= round(testAccuracy$accuracy()$getInfo(), digits=4)
+        ntree = combinations_k[k,]$ntree,
+        mtry = combinations_k[k,]$mtry,
+        region = unique(combinations$region)[i],
+        year = unique(combinations$year)[j],
+        accuracy = round(testAccuracy$accuracy()$getInfo(), digits=4)
       ))), silent= T)
       
-      if(exists('tempParam') == FALSE) {
-        ## get end time and estimated time to end
+      ## If accuracy results are not available, continue to the next iteration
+      if (!exists('tempParam')) {
         endTime <- Sys.time()
-        print(paste0('Task Runtime: ', round(endTime - startTime, digits=1),'s -------------> Estimated to end all tasks: ',
+        print(paste0('Task Runtime: ', round(endTime - startTime, digits=1),'s ----> Estimated time to finish: ',
                      round((endTime - startTime) * nrow(combinations)/3600, digits=1), ' hours'))
         next
-        
       }
       
-      ## temp iomportance
+      ## Store feature importance for the current parameter combination
       try(tempImportance <- as.data.frame(rbind(cbind(
-        ntree= combinations_k[k,]$ntree,
-        mtry= combinations_k[k,]$mtry,
-        region= unique(combinations$region)[i],
-        year= unique(combinations$year)[j],
-        bandNames= bands[bands != 'reference'],
-        importance= as.numeric(unlist(trainedClassifier$explain()$get('importance')$getInfo()))
+        ntree = combinations_k[k,]$ntree,
+        mtry = combinations_k[k,]$mtry,
+        region = unique(combinations$region)[i],
+        year = unique(combinations$year)[j],
+        bandNames = bands[bands != 'reference'],
+        importance = as.numeric(unlist(trainedClassifier$explain()$get('importance')$getInfo()))
       ))), silent= T)
       
-      if(exists('tempImportance') == FALSE) {
-        ## get end time and estimated time to end
+      ## If importance results are not available, continue to the next iteration
+      if (!exists('tempImportance')) {
         endTime <- Sys.time()
-        print(paste0('Task Runtime: ', round(endTime - startTime, digits=1),'s -------------> Estimated to end all tasks: ',
+        print(paste0('Task Runtime: ', round(endTime - startTime, digits=1),'s ----> Estimated time to finish: ',
                      round((endTime - startTime) * nrow(combinations)/3600, digits=1), ' hours'))
         next
-        
       }
       
-      ## bind data
+      ## Append results to the parameter and importance tables
       paramTable <- rbind(paramTable, tempParam)
       importanceTable <- rbind(importanceTable, tempImportance)
       
-      ## get end time and estimated time to end
+      ## Record the end time and estimated remaining time
       endTime <- Sys.time()
-      print(paste0('Task Runtime: ', round(endTime - startTime, digits=1),'s -------------> Estimated to end all tasks: ',
+      print(paste0('Task Runtime: ', round(endTime - startTime, digits=1),'s ----> Estimated time to finish: ',
                    round((endTime - startTime) * nrow(combinations)/3600, digits=1), ' hours'))
       
+      ## Clean up temporary variables
       rm(tempParam)
       rm(tempImportance)
       
@@ -178,6 +189,6 @@ for (i in 1:length(unique(combinations$region))) {
   
 }
 
-## save data locally to be used 
+## Save the parameter and importance data locally for further analysis
 write.table(paramTable, file = './_aux/modelParams.csv', row.names= FALSE)
 write.table(importanceTable, file = './_aux/varImportance.csv', row.names=FALSE)
